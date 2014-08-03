5e865992a7122456151bf3f3407212e40d822c1a
Lucien Jarrett <lucien.jarrett@gmail.com>
2014-08-02 19:22:34 -0500

    modify model #5

--- app/models/employee_vacation.rb.BACKUP.4494.rb
+++
@@ -1,9 +0,0 @@
-class EmployeeVacation < ActiveRecord::Base
-<<<<<<< HEAD
-  
-  belongs_to :vacation 
-  belongs_to :employee
-  accepts_nested_attributes_for :vacation
-=======
->>>>>>> develope
-end
--- app/models/employee_vacation.rb.LOCAL.4494.rb
+++
@@ -1,6 +0,0 @@
-class EmployeeVacation < ActiveRecord::Base
-  
-  belongs_to :vacation 
-  belongs_to :employee
-  accepts_nested_attributes_for :vacation
-end
