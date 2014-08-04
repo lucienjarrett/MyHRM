c316c8c5283a7498791896c368572602e1e887ff
Lucien Jarrett <lucien.jarrett@gmail.com>
2014-08-04 17:38:39 -0500

    add semantic ui

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,14 @@ class UsersController < ApplicationController
   def create 
     @user = User.new(user_params)
     if @user.save 
-      session[:user_id] = @user.id
+      cookies[:auth_token] =  @user.generate_token(:auth_token)
       redirect_to root_url, notice: "Thank you for signing up.."
     else 
       render "new"
     end 
   end 
   
+  private 
   def user_params
        params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,26 @@
 class User < ActiveRecord::Base
   has_secure_password 
-  validates_uniqueness_of :email
- # validates_format_of :email, :with => /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
+  before_create { generate_token(:auth_token) }
   
-  
-  def generate_token 
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+    validates :password, presence: true, :on => :create
+    validates :password, length: { minimum: 6 }, :allow_blank => true
+    validates :password_confirmation, presence: true, :on => :create
+    validates_uniqueness_of :email
+    validates_presence_of :password, :on => :create
+ 
+
+  def send_password_reset 
+      generate_token(:password_reset_token)
+      self.password_reset_sent_at = Time.zone.now 
+      save!
+      UserMailer.password_reset(self).deliver
+  end
+
+  def generate_token(column) 
     begin 
       self[column] = SecureRandom.urlsafe_base64
     end while User.exists?(column => self[column])
-    
-  end 
-  
-  def send_password_reset 
-    generate_token(:password_reset_token)
-    self.password_reset_sent_at = Time.zone.now 
-    save!
-    UserMailer.password_reset(self).deliver
-  end 
-  
+  end  
 end
